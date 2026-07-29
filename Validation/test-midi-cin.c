torvalds/GuitarPedal

commit 95881ae06f8bfeab006c3dab893a55711342c191

Re-organize MIDI code a bit, add UART option
This adds a parallel path for MIDI packets using the UART for
traditional HW MIDI ports.

It's off by default, because I didn't think things through: my non-UI
pedal with MIDI HW support uses GPIO26/27 for MIDI OUT/IN.

But that's what the second rotary encoder uses for the pedal with a UI,
so if you enable MIDI support you lose the rotary.

So make it an option you have to explicitly enable when you build for
the non-UI pedal.

I didn't bring out enough GPIOs from the rp2354 board to have unique
GPIOs (mainly because I incorrectly thought that I wanted to bring out
the SWD pins that I've never used), so now I have the situation that I
have to make custom configs for each board.

Oh well.  This is certainly not unusual with MCU boards, but it's just
slightly annoying nonetheless.

Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

Software/midi.h
@@ -1,0 +1,62 @@
+//
+// MIDI connection code
+//
+
+// MIDI Control Change (CC) constants
+#define MIDI_CC_POT_START        10 // CC 10-19 map to pots 0-9
+#define MIDI_CC_GLOBAL_ENABLE    20
+#define MIDI_CC_EFFECT_ENABLE    21
+#define MIDI_CC_ACTIVE_POT       22
+
+#define MIDI_CC_EFFECT_INTENSE   30
+#define MIDI_CC_AUDIO_CLIPPING   31
+#define MIDI_CC_CPU_LATENCY      32
+
+extern int current_midi_effect_idx;
+
+void handle_midi_packet(const uint8_t packet[4]);
+void usb_midi_write(const uint8_t packet[4]);
+void uart_midi_write(const uint8_t packet[4]);
+
+static inline void send_midi_cc(uint8_t cc, uint8_t val)
+{
+	uint8_t packet[4] = { 0x0B, 0xB0, cc, val };
+	usb_midi_write(packet);
+	uart_midi_write(packet);
+}
+
+static inline void send_midi_pc(uint8_t pc)
+{
+	uint8_t packet[4] = { 0x0C, 0xC0, pc, 0 };
+	usb_midi_write(packet);
+	uart_midi_write(packet);
+}
+
+static inline void send_midi_note_on(uint8_t ch, uint8_t note, uint8_t vel)
+{
+	uint8_t packet[4] = { 0x09, 0x90 | (ch & 0x0F), note, vel };
+	usb_midi_write(packet);
+	uart_midi_write(packet);
+}
+
+static inline void send_midi_note_off(uint8_t ch, uint8_t note, uint8_t vel)
+{
+	uint8_t packet[4] = { 0x08, 0x80 | (ch & 0x0F), note, vel };
+	usb_midi_write(packet);
+	uart_midi_write(packet);
+}
+
+static inline void send_midi_pitch_bend(uint8_t ch, int16_t bend)
+{
+	uint16_t val = bend + 8192;
+	uint8_t packet[4] = { 0x0E, 0xE0 | (ch & 0x0F), val & 0x7F, (val >> 7) & 0x7F };
+	usb_midi_write(packet);
+	uart_midi_write(packet);
+}
+
+static inline void send_midi_channel_pressure(uint8_t ch, uint8_t pressure)
+{
+	uint8_t packet[4] = { 0x0D, 0xD0 | (ch & 0x0F), pressure & 0x7F, 0 };
+	usb_midi_write(packet);
+	uart_midi_write(packet);
+}

Software/uart.h
@@ -1,0 +1,125 @@
+#if MIDI_HW
+#define UART_TX_BUF_SIZE 512
+static uint8_t uart_tx_buf[UART_TX_BUF_SIZE];
+static volatile unsigned uart_tx_head;
+static volatile unsigned uart_tx_tail;
+
+#define UART_RX_BUF_SIZE 256
+static uint8_t uart_rx_buf[UART_RX_BUF_SIZE];
+static volatile unsigned uart_rx_head;
+static volatile unsigned uart_rx_tail;
+#endif
+
+void uart_midi_write(const uint8_t packet[4])
+{
+#if MIDI_HW
+	uint8_t cin = packet[0] & 0x0F;
+	int len = 0;
+	switch (cin) {
+		case 0x8: case 0x9: case 0xB: case 0xE: len = 3; break;
+		case 0xC: case 0xD: len = 2; break;
+		default: len = 0; break;
+	}
+	for (int i = 0; i < len; i++) {
+		unsigned head = uart_tx_head;
+		unsigned next_head = (head + 1) % UART_TX_BUF_SIZE;
+		if (next_head != uart_tx_tail) {
+			uart_tx_buf[head] = packet[1 + i];
+			uart_tx_head = next_head;
+		}
+	}
+#endif
+}
+
+bool uart_midi_read(uint8_t packet[4])
+{
+#if MIDI_HW
+	static int expected_bytes = 0;
+	static uint8_t parser_packet[4];
+	static int parser_idx = 0;
+
+	while (uart_rx_head != uart_rx_tail) {
+		uint8_t b = uart_rx_buf[uart_rx_tail];
+		uart_rx_tail = (uart_rx_tail + 1) % UART_RX_BUF_SIZE;
+
+		if (b >= 0xF8) {
+			// Real-time message
+			continue;
+		} else if (b >= 0x80) {
+			parser_packet[1] = b;
+			parser_idx = 2;
+			if ((b & 0xF0) == 0xC0 || (b & 0xF0) == 0xD0) {
+				expected_bytes = 1;
+			} else if (b < 0xF0) {
+				expected_bytes = 2;
+			} else {
+				expected_bytes = 0;
+			}
+		} else if (expected_bytes > 0 && parser_idx > 0) {
+			parser_packet[parser_idx++] = b;
+			if (parser_idx - 2 == expected_bytes) {
+				packet[0] = 0;
+				packet[1] = parser_packet[1];
+				packet[2] = parser_packet[2];
+				packet[3] = parser_packet[3];
+				parser_idx = 2;
+				return true;
+			}
+		}
+	}
+#endif
+	return false;
+}
+
+void uart_midi_poll(void)
+{
+#if MIDI_HW
+	while (uart_is_readable(MIDI_UART)) {
+		unsigned head = uart_rx_head;
+		unsigned next_head = (head + 1) % UART_RX_BUF_SIZE;
+		if (next_head != uart_rx_tail) {
+			uart_rx_buf[head] = uart_getc(MIDI_UART);
+			uart_rx_head = next_head;
+		} else {
+			break;
+		}
+	}
+
+	while (uart_tx_head != uart_tx_tail && uart_is_writable(MIDI_UART)) {
+		uart_putc_raw(MIDI_UART, uart_tx_buf[uart_tx_tail]);
+		uart_tx_tail = (uart_tx_tail + 1) % UART_TX_BUF_SIZE;
+	}
+
+	uint8_t packet[4];
+	while (uart_midi_read(packet))
+		handle_midi_packet(packet);
+#endif
+}
+
+static void uart_midi_init(void)
+{
+#if MIDI_HW
+	// On RP2350, function 11 maps GPIO 26/27 to UART1 TX/RX
+	// The normal UART_FUNCSEL_NUM macro can't deal with that
+	//
+	// Don't even ask how long it took to debug this: I had
+	// read the datasheet when setting this all up, but I
+	// hadn't connected the dots on UART_FUNCSEL_NUM() not
+	// doing the right thing.
+	//
+	// There is probably some proper way to do this in the
+	// SDK, but whatever.
+	gpio_set_function(MIDI_OUT, 11);
+	gpio_set_function(MIDI_IN, 11);
+
+	// MIDI idle is +5V, but that is "no current": LED is off,
+	// and the TLP2310 drives the MIDI_IN pin low.
+	//
+	// Standard UART idle is high, but that is easily dealt
+	// with by just inverting the GPIO pin
+	gpio_set_inover(MIDI_IN, GPIO_OVERRIDE_INVERT);
+
+	// Let it rip!
+	uart_init(MIDI_UART, 31250);
+#endif
+}

Software/CMakeLists.txt
@@ -11,6 +11,13 @@ pico_sdk_init()
 
 add_executable(blink)
 
+# I should have used different PIO pins for MIDI
+# but they conflict with the second rotary encoder
+set(MIDI_HW OFF CACHE BOOL "Enable MIDI HW (not just MIDI over USB-C) support")
+
+set(USB_MODE "DEVICE" CACHE STRING "USB Mode: DEVICE (default) or HOST")
+set_property(CACHE USB_MODE PROPERTY STRINGS DEVICE HOST)
+
 set(SYS_CLK_KHZ 172800)
 set(SAMPLES_PER_SEC 48000.0)
 set(SCRIPT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/scripts")
@@ -85,10 +92,8 @@ pico_generate_pio_header(blink ${PIO_DIR}/debounce.pio)
 pico_generate_pio_header(blink ${PIO_DIR}/rotary.pio)
 pico_generate_pio_header(blink ${PIO_DIR}/i2s.pio)
 
-set(USB_MODE "DEVICE" CACHE STRING "USB Mode: DEVICE (default) or HOST")
-set_property(CACHE USB_MODE PROPERTY STRINGS DEVICE HOST)
-
 target_compile_definitions(blink PRIVATE
+	MIDI_HW=$<BOOL:${MIDI_HW}>
 	USB_MODE_${USB_MODE}=1
 	SAMPLES_PER_SEC=${SAMPLES_PER_SEC}f
 	CFG_TUSB_MCU=OPT_MCU_RP2040

Software/blink.c
@@ -12,8 +12,6 @@
 
 #include "board.h"
 
-#include "usb-sync.h"
-
 #include "ws2812.pio.h"
 #include "debounce.pio.h"
 #include "rotary.pio.h"
@@ -32,6 +30,8 @@
 #include "audio/fft.h"
 #include "audio/analyze.h"
 
+#include "midi.h"
+#include "uart.h"
 #include "tusb.h"
 #include "usb-audio.h"
 #include "sh1106.h"
@@ -430,9 +430,11 @@ static void init_rotary_encoder(void)
 	init_sw_pin(pio, GPIO_ROT1B);
 	rotary_program_init(pio, 0, offset, GPIO_ROT1A);
 
+#if !MIDI_HW
 	init_sw_pin(pio, GPIO_ROT2A);
 	init_sw_pin(pio, GPIO_ROT2B);
 	rotary_program_init(pio, 1, offset, GPIO_ROT2A);
+#endif
 
 	irq_set_exclusive_handler(PIO2_IRQ_0, rotary_irq);
 	irq_set_enabled(PIO2_IRQ_0, true);
@@ -521,6 +523,7 @@ int main()
 	init_i2c_bus(i2c1, 400, I2C1_SDA, I2C1_SCL);
 
 	init_usb();
+	uart_midi_init();
 
 	absolute_time_t now = get_absolute_time();
 
@@ -550,6 +553,7 @@ int main()
 		usb_audio_task();
 #endif
 		sh1106_task();
+		uart_midi_poll();
 
 		// Claim 25Hz screen updates
 		if (now > next_ui_update) {

Software/include/board.h
@@ -28,8 +28,17 @@
 #define GPIO_ROT1A		6
 #define GPIO_ROT1B		7
 
-#define GPIO_ROT2A		26
-#define GPIO_ROT2B		27
+// The MIDI-controlled ones do not have
+// the second rotary encoder
+#if MIDI_HW
+  #define MIDI_OUT		26
+  #define MIDI_IN		27
+  #define MIDI_UART		uart1
+#else
+  #define GPIO_ROT2A		26
+  #define GPIO_ROT2B		27
+#endif
+
 
 #define I2C0_SDA		4
 #define I2C0_SCL		5

Software/usb-device.c
@@ -4,8 +4,9 @@
 #include "board.h"
 #include "tusb.h"
 
+#include "midi.h"
+
 #include "usb-audio.h"
-#include "usb-sync.h"
 
 //--------------------------------------------------------------------+
 // Device Descriptors
@@ -510,8 +511,6 @@ float get_usb_audio_input(void)
 	return 0.0f;
 }
 
-extern void handle_midi_packet(const uint8_t packet[4]);
-
 void tud_midi_rx_cb(uint8_t itf)
 {
 	(void)itf;
@@ -521,39 +520,8 @@ void tud_midi_rx_cb(uint8_t itf)
 	}
 }
 
-void send_midi_cc(uint8_t cc, uint8_t val)
+void usb_midi_write(const uint8_t packet[4])
 {
-	uint8_t packet[4] = { 0x0B, 0xB0, cc, val };
-	if (tud_midi_mounted()) tud_midi_packet_write(packet);
-}
-
-void send_midi_pc(uint8_t pc)
-{
-	uint8_t packet[4] = { 0x0C, 0xC0, pc, 0 };
-	if (tud_midi_mounted()) tud_midi_packet_write(packet);
-}
-
-void send_midi_note_on(uint8_t ch, uint8_t note, uint8_t vel)
-{
-	uint8_t packet[4] = { 0x09, 0x90 | (ch & 0x0F), note, vel };
-	if (tud_midi_mounted()) tud_midi_packet_write(packet);
-}
-
-void send_midi_note_off(uint8_t ch, uint8_t note, uint8_t vel)
-{
-	uint8_t packet[4] = { 0x08, 0x80 | (ch & 0x0F), note, vel };
-	if (tud_midi_mounted()) tud_midi_packet_write(packet);
-}
-
-void send_midi_pitch_bend(uint8_t ch, int16_t bend)
-{
-	uint16_t val = bend + 8192;
-	uint8_t packet[4] = { 0x0E, 0xE0 | (ch & 0x0F), val & 0x7F, (val >> 7) & 0x7F };
-	if (tud_midi_mounted()) tud_midi_packet_write(packet);
-}
-
-void send_midi_channel_pressure(uint8_t ch, uint8_t pressure)
-{
-	uint8_t packet[4] = { 0x0D, 0xD0 | (ch & 0x0F), pressure & 0x7F, 0 };
-	if (tud_midi_mounted()) tud_midi_packet_write(packet);
+	if (tud_midi_mounted())
+		tud_midi_packet_write(packet);
 }

Software/usb-host.c
@@ -1,8 +1,7 @@
 #include "pico/stdlib.h"
 #include "tusb.h"
-#include "usb-sync.h"
 
-extern void handle_midi_packet(const uint8_t packet[4]);
+#include "midi.h"
 
 // Needed because audio/effect.h expects it
 float get_usb_audio_input(void)
@@ -40,56 +39,9 @@ void tuh_midi_umount_cb(uint8_t idx)
 	}
 }
 
-void send_midi_cc(uint8_t cc, uint8_t val)
+void usb_midi_write(const uint8_t packet[4])
 {
 	if (sync_connected) {
-		uint8_t packet[4] = { 0x0B, 0xB0, cc, val };
-		tuh_midi_packet_write(sync_idx, packet);
-		tuh_midi_write_flush(sync_idx);
-	}
-}
-
-void send_midi_pc(uint8_t pc)
-{
-	if (sync_connected) {
-		uint8_t packet[4] = { 0x0C, 0xC0, pc, 0 };
-		tuh_midi_packet_write(sync_idx, packet);
-		tuh_midi_write_flush(sync_idx);
-	}
-}
-
-void send_midi_note_on(uint8_t ch, uint8_t note, uint8_t vel)
-{
-	if (sync_connected) {
-		uint8_t packet[4] = { 0x09, 0x90 | (ch & 0x0F), note, vel };
-		tuh_midi_packet_write(sync_idx, packet);
-		tuh_midi_write_flush(sync_idx);
-	}
-}
-
-void send_midi_note_off(uint8_t ch, uint8_t note, uint8_t vel)
-{
-	if (sync_connected) {
-		uint8_t packet[4] = { 0x08, 0x80 | (ch & 0x0F), note, vel };
-		tuh_midi_packet_write(sync_idx, packet);
-		tuh_midi_write_flush(sync_idx);
-	}
-}
-
-void send_midi_pitch_bend(uint8_t ch, int16_t bend)
-{
-	if (sync_connected) {
-		uint16_t val = bend + 8192;
-		uint8_t packet[4] = { 0x0E, 0xE0 | (ch & 0x0F), val & 0x7F, (val >> 7) & 0x7F };
-		tuh_midi_packet_write(sync_idx, packet);
-		tuh_midi_write_flush(sync_idx);
-	}
-}
-
-void send_midi_channel_pressure(uint8_t ch, uint8_t pressure)
-{
-	if (sync_connected) {
-		uint8_t packet[4] = { 0x0D, 0xD0 | (ch & 0x0F), pressure & 0x7F, 0 };
 		tuh_midi_packet_write(sync_idx, packet);
 		tuh_midi_write_flush(sync_idx);
 	}

Software/usb-sync.h
@@ -1,28 +1,0 @@
-#ifndef _USB_SYNC_H
-#define _USB_SYNC_H
-
-#include <stdint.h>
-
-// MIDI Control Change (CC) constants
-#define MIDI_CC_POT_START        10 // CC 10-19 map to pots 0-9
-#define MIDI_CC_GLOBAL_ENABLE    20
-#define MIDI_CC_EFFECT_ENABLE    21
-#define MIDI_CC_ACTIVE_POT       22
-
-#define MIDI_CC_EFFECT_INTENSE   30
-#define MIDI_CC_AUDIO_CLIPPING   31
-#define MIDI_CC_CPU_LATENCY      32
-
-// MIDI Program Change maps to effect_idx directly (0-N)
-
-extern void send_midi_cc(uint8_t cc, uint8_t val);
-extern void send_midi_pc(uint8_t pc);
-
-extern void send_midi_note_on(uint8_t ch, uint8_t note, uint8_t vel);
-extern void send_midi_note_off(uint8_t ch, uint8_t note, uint8_t vel);
-extern void send_midi_pitch_bend(uint8_t ch, int16_t bend);
-extern void send_midi_channel_pressure(uint8_t ch, uint8_t pressure);
-
-extern int current_midi_effect_idx;
-
-#endif // _USB_SYNC_H

commit 694a4015710ae6bba4637be266d40e9b1084018e

MIDI: parse the hardware port as a byte stream
The screenless pedal is meant to be programmed over either USB or its
TRS Type-A MIDI ports, and the scene protocol now carries its schema,
state and routing commands over SysEx.  The transmit side learned all
the USB-MIDI CIN lengths in commit db24ff61829d ("Get the USB-MIDI code
index numbers right in both directions"), but uart_midi_read() still
treats F0-F7 as zero-length status bytes.  A SysEx request arriving at
the TRS input disappears before handle_midi_packet() sees it.

The same reader recognizes only channel voice messages.  It drops
every real-time byte, so MIDI Clock arriving at the TRS jack never
reaches USB, and System Common messages disappear too.  A two-byte
channel message also leaves the final packet byte holding data from an
earlier three-byte event.

Replace that ad-hoc reader with a byte-stream parser.  Keep channel
running status, emit real-time without disturbing a partly read message,
handle System Common, and pack SysEx into the CIN 4..7 forms USB-MIDI
needs.  System Reset is the real-time exception and clears the parser
state.  Zero-pad every packet byte that the MIDI message does not use.

Keep the parser next to the CIN helpers so the normally-disabled
MIDI_HW path has host-side coverage.  Feed it channel running status,
real-time interleaved with both channel and SysEx messages, System
Common, System Reset, and all three SysEx ending forms.

Fixes: 95881ae06f8b ("Re-organize MIDI code a bit, add UART option")
Assisted-by: Codex:gpt-5
Signed-off-by: diegoMalagrida <diegomalagrida@gmail.com>

Software/midi.h
@@ -74,6 +74,127 @@ static inline uint8_t midi_status_cin(uint8_t status)
 	return status >> 4;
 }
 
+//
+// Turn a raw MIDI byte stream into USB-MIDI event packets.
+//
+// The UART sees the wire format, with running status and real-time bytes
+// interspersed anywhere in another message.  USB-MIDI wants self-contained
+// packets instead.  Keep the parser here with the CIN helpers: the mapping
+// is part of parsing a byte stream, and putting it in a host-side test means
+// the MIDI_HW-only path is exercised too.
+//
+struct midi_stream_parser {
+	uint8_t bytes[3];
+	int nr_bytes;
+	int message_len;
+	bool in_sysex;
+};
+
+static inline void midi_stream_reset(struct midi_stream_parser *parser)
+{
+	parser->nr_bytes = 0;
+	parser->message_len = 0;
+	parser->in_sysex = false;
+}
+
+static inline void midi_stream_packet(uint8_t packet[4], uint8_t cin,
+	const uint8_t bytes[3], int nr_bytes)
+{
+	packet[0] = cin;
+	packet[1] = nr_bytes > 0 ? bytes[0] : 0;
+	packet[2] = nr_bytes > 1 ? bytes[1] : 0;
+	packet[3] = nr_bytes > 2 ? bytes[2] : 0;
+}
+
+//
+// Feed one byte of a raw MIDI stream.  Return true when it completes one
+// USB-MIDI packet in 'packet'.  Real-time messages are emitted immediately
+// without disturbing a partly received channel message or SysEx stream.
+// System Reset is the exception: it also clears the parser state.
+//
+static inline bool midi_stream_read(struct midi_stream_parser *parser,
+	uint8_t b, uint8_t packet[4])
+{
+	if (b >= 0xF8) {
+		uint8_t byte[] = { b, 0, 0 };
+		midi_stream_packet(packet, 0x0F, byte, 1);
+		if (b == 0xFF)
+			midi_stream_reset(parser);
+		return true;
+	}
+
+	if (parser->in_sysex) {
+		if (b == 0xF7) {
+			parser->bytes[parser->nr_bytes++] = b;
+			midi_stream_packet(packet,
+				(uint8_t)(0x04 + parser->nr_bytes),
+				parser->bytes, parser->nr_bytes);
+			parser->in_sysex = false;
+			parser->nr_bytes = 0;
+			return true;
+		}
+		if (b < 0x80) {
+			parser->bytes[parser->nr_bytes++] = b;
+			if (parser->nr_bytes != 3)
+				return false;
+			midi_stream_packet(packet, 0x04, parser->bytes, 3);
+			parser->nr_bytes = 0;
+			return true;
+		}
+
+		// A non-real-time status byte abandons an unfinished SysEx.
+		parser->in_sysex = false;
+		parser->nr_bytes = 0;
+	}
+
+	if (b < 0x80) {
+		if (!parser->message_len)
+			return false;
+
+		parser->bytes[parser->nr_bytes++] = b;
+		if (parser->nr_bytes != parser->message_len)
+			return false;
+
+		midi_stream_packet(packet, midi_status_cin(parser->bytes[0]),
+			parser->bytes, parser->nr_bytes);
+
+		// Channel messages keep their status for MIDI running status.
+		if (parser->bytes[0] < 0xF0)
+			parser->nr_bytes = 1;
+		else
+			midi_stream_reset(parser);
+		return true;
+	}
+
+	parser->nr_bytes = 0;
+	parser->message_len = 0;
+	if (b == 0xF0) {
+		parser->bytes[parser->nr_bytes++] = b;
+		parser->in_sysex = true;
+		return false;
+	}
+
+	parser->bytes[parser->nr_bytes++] = b;
+	if (b < 0xF0)
+		parser->message_len = (b & 0xF0) == 0xC0 ||
+			(b & 0xF0) == 0xD0 ? 2 : 3;
+	else if (b == 0xF1 || b == 0xF3)
+		parser->message_len = 2;
+	else if (b == 0xF2)
+		parser->message_len = 3;
+	else if (b >= 0xF4 && b <= 0xF7)
+		parser->message_len = 1;
+	else
+		return false;
+
+	if (parser->message_len != 1)
+		return false;
+
+	midi_stream_packet(packet, midi_status_cin(b), parser->bytes, 1);
+	midi_stream_reset(parser);
+	return true;
+}
+
 bool handle_midi_packet(const uint8_t packet[4]);
 void usb_midi_poll(void);
 bool usb_midi_write(const uint8_t packet[4]);

Software/uart.h
@@ -29,41 +29,14 @@ void uart_midi_write(const uint8_t packet[4])
 bool uart_midi_read(uint8_t packet[4])
 {
 #if MIDI_HW
-	static int expected_bytes = 0;
-	static uint8_t parser_packet[4];
-	static int parser_idx = 0;
+	static struct midi_stream_parser parser;
 
 	while (uart_rx_head != uart_rx_tail) {
 		uint8_t b = uart_rx_buf[uart_rx_tail];
 		uart_rx_tail = (uart_rx_tail + 1) % UART_RX_BUF_SIZE;
 
-		if (b >= 0xF8) {
-			// Real-time message
-			continue;
-		} else if (b >= 0x80) {
-			parser_packet[1] = b;
-			parser_idx = 2;
-			if ((b & 0xF0) == 0xC0 || (b & 0xF0) == 0xD0) {
-				expected_bytes = 1;
-			} else if (b < 0xF0) {
-				expected_bytes = 2;
-			} else {
-				expected_bytes = 0;
-			}
-		} else if (expected_bytes > 0 && parser_idx > 0) {
-			parser_packet[parser_idx++] = b;
-			if (parser_idx - 2 == expected_bytes) {
-				// CIN 0 is reserved: a host is entitled to
-				// ignore it, and ours was emitting nothing
-				// else on this path.
-				packet[0] = midi_status_cin(parser_packet[1]);
-				packet[1] = parser_packet[1];
-				packet[2] = parser_packet[2];
-				packet[3] = parser_packet[3];
-				parser_idx = 2;
-				return true;
-			}
-		}
+		if (midi_stream_read(&parser, b, packet))
+			return true;
 	}
 #endif
 	return false;

Validation/test-midi-cin.c
@@ -21,6 +21,32 @@ static void chk(const char *what, int got, int want)
 	}
 }
 
+static void chk_packet(const char *what, const uint8_t packet[4],
+	uint8_t cin, uint8_t b1, uint8_t b2, uint8_t b3)
+{
+	if (packet[0] != cin || packet[1] != b1 || packet[2] != b2 ||
+	    packet[3] != b3) {
+		printf("FAIL %-28s got %02x %02x %02x %02x want %02x %02x %02x %02x\n",
+			what, packet[0], packet[1], packet[2], packet[3],
+			cin, b1, b2, b3);
+		fails++;
+	}
+}
+
+static void chk_stream_packet(struct midi_stream_parser *parser,
+	uint8_t b, const char *what, uint8_t cin, uint8_t b1, uint8_t b2,
+	uint8_t b3)
+{
+	uint8_t packet[4];
+
+	if (!midi_stream_read(parser, b, packet)) {
+		printf("FAIL %-28s no packet\n", what);
+		fails++;
+		return;
+	}
+	chk_packet(what, packet, cin, b1, b2, b3);
+}
+
 int main(void)
 {
 	// Channel voice: CIN is the top nibble, and the lengths are the
@@ -69,6 +95,59 @@ int main(void)
 	chk("reserved 0", midi_cin_length(0x0), 0);
 	chk("reserved 1", midi_cin_length(0x1), 0);
 
-	printf(fails ? "%d failures\n" : "all CIN checks pass\n", fails);
+	// The DIN port receives a byte stream, not USB-MIDI packets. Check
+	// that the stream parser preserves running status, emits real-time
+	// messages in the middle of another message, and packs SysEx endings
+	// with the right number of bytes.
+	struct midi_stream_parser parser;
+	midi_stream_reset(&parser);
+	chk("note status", midi_stream_read(&parser, 0x90, (uint8_t[4]) { 0 }), false);
+	chk("note data 1", midi_stream_read(&parser, 0x3C, (uint8_t[4]) { 0 }), false);
+	chk_stream_packet(&parser, 0xF8, "clock between note bytes", 0x0F, 0xF8, 0, 0);
+	chk_stream_packet(&parser, 0x40, "note after clock", 0x09, 0x90, 0x3C, 0x40);
+	chk("running data 1", midi_stream_read(&parser, 0x3D, (uint8_t[4]) { 0 }), false);
+	chk_stream_packet(&parser, 0x41, "running note", 0x09, 0x90, 0x3D, 0x41);
+
+	midi_stream_reset(&parser);
+	chk("program status", midi_stream_read(&parser, 0xC0, (uint8_t[4]) { 0 }), false);
+	chk_stream_packet(&parser, 0x05, "program change", 0x0C, 0xC0, 0x05, 0);
+	chk_stream_packet(&parser, 0x06, "running program", 0x0C, 0xC0, 0x06, 0);
+
+	// System Reset is real-time, but unlike the other real-time messages
+	// it resets receivers to power-up state and therefore cancels both
+	// a message in progress and running status.
+	midi_stream_reset(&parser);
+	chk("reset note status", midi_stream_read(&parser, 0x90, (uint8_t[4]) { 0 }), false);
+	chk("reset note data 1", midi_stream_read(&parser, 0x3C, (uint8_t[4]) { 0 }), false);
+	chk_stream_packet(&parser, 0xFF, "system reset", 0x0F, 0xFF, 0, 0);
+	chk("data after reset", midi_stream_read(&parser, 0x40, (uint8_t[4]) { 0 }), false);
+
+	midi_stream_reset(&parser);
+	chk("song position status", midi_stream_read(&parser, 0xF2, (uint8_t[4]) { 0 }), false);
+	chk("song position data 1", midi_stream_read(&parser, 0x01, (uint8_t[4]) { 0 }), false);
+	chk_stream_packet(&parser, 0x02, "song position", 0x03, 0xF2, 0x01, 0x02);
+	chk_stream_packet(&parser, 0xF6, "tune request", 0x05, 0xF6, 0, 0);
+	chk_stream_packet(&parser, 0xF7, "standalone EOX", 0x05, 0xF7, 0, 0);
+
+	midi_stream_reset(&parser);
+	chk("sysex f0", midi_stream_read(&parser, 0xF0, (uint8_t[4]) { 0 }), false);
+	chk("sysex byte 1", midi_stream_read(&parser, 0x7D, (uint8_t[4]) { 0 }), false);
+	chk_stream_packet(&parser, 0xF8, "clock inside sysex", 0x0F, 0xF8, 0, 0);
+	chk_stream_packet(&parser, 0x03, "sysex start", 0x04, 0xF0, 0x7D, 0x03);
+	chk("sysex byte 2", midi_stream_read(&parser, 0x12, (uint8_t[4]) { 0 }), false);
+	chk_stream_packet(&parser, 0xF7, "sysex end 2", 0x06, 0x12, 0xF7, 0);
+
+	midi_stream_reset(&parser);
+	chk("short sysex f0", midi_stream_read(&parser, 0xF0, (uint8_t[4]) { 0 }), false);
+	chk("short sysex byte", midi_stream_read(&parser, 0x7D, (uint8_t[4]) { 0 }), false);
+	chk_stream_packet(&parser, 0xF7, "sysex end 3", 0x07, 0xF0, 0x7D, 0xF7);
+
+	midi_stream_reset(&parser);
+	chk("long sysex f0", midi_stream_read(&parser, 0xF0, (uint8_t[4]) { 0 }), false);
+	chk("long sysex byte 1", midi_stream_read(&parser, 0x01, (uint8_t[4]) { 0 }), false);
+	chk_stream_packet(&parser, 0x02, "long sysex start", 0x04, 0xF0, 0x01, 0x02);
+	chk_stream_packet(&parser, 0xF7, "sysex end 1", 0x05, 0xF7, 0, 0);
+
+	printf(fails ? "%d failures\n" : "all MIDI checks pass\n", fails);
 	return !!fails;
 }